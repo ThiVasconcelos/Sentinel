85ed04c8fb35de483516a579f45dfd24cdeade1d
Thiago Vasconcelos <114086890+ThiVasconcelos@users.noreply.github.com>
2024-05-05 17:58:25 +0100

    ajuste app.modules.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegramController } from './features/telegram/telegram.controller';
-import { TelegramController } from './features/telegram/telegram.controller';
+
 
 @Module({
   imports: [],
