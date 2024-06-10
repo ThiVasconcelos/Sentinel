a9ae44dc8639662fd3c861d3fccabd2ed1950082
Thiago Vasconcelos <114086890+ThiVasconcelos@users.noreply.github.com>
2024-06-10 11:21:05 +0100

    build

--- frontend/src/app/api/sentinel.api.ts
+++ frontend/src/app/api/sentinel.api.ts
@@ -2,32 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ChatInfo, UserBadWordObject } from '../types/sentinel.types';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SentinelApi {
   constructor(private http: HttpClient) {}
 
   hasAnotherInfractionOccurred(lastDate: Date) {
-    return this.http.get<{ occurred: boolean }>('http://localhost:3000/admin/notification', {
+    return this.http.get<{ occurred: boolean }>(`${environment.apiUrl}/admin/notification`, {
       params: { date: lastDate.toISOString() },
     });
   }
 
   getChatsData(): Observable<{ data: UserBadWordObject[]; chats: ChatInfo[] }> {
     return this.http.get<{ data: UserBadWordObject[]; chats: ChatInfo[] }>(
-      'http://localhost:3000/admin/get-data'
+      `${environment.apiUrl}/admin/get-data`
     );
   }
 
   freeUser(userData: UserBadWordObject) {
-    return this.http.post('http://localhost:3000/admin/free-user/', userData);
+    return this.http.post(`${environment.apiUrl}/admin/free-user/`, userData);
   }
 
   banUser(userData: UserBadWordObject) {
-    return this.http.post('http://localhost:3000/admin/ban-user/', userData);
+    return this.http.post(`${environment.apiUrl}/admin/ban-user/`, userData);
   }
 
   timeoutUser(userData: UserBadWordObject) {
-    return this.http.post('http://localhost:3000/admin/timeout-user/', userData);
+    return this.http.post(`${environment.apiUrl}/admin/timeout-user/`, userData);
   }
 }
